95b64ce893d137bc6995eb6231be245c68749350
AzizAub <azizaubakirov00@gmail.com>
2023-05-30 22:32:51 +0200

    create function for even or odd differentiation

---
+++ assignment1_2.py
@@ -0,0 +1,14 @@
+def even_or_odd(lst) -> tuple:
+    even_numb = []
+    odd_numb = []
+    for numb in lst:
+        if numb % 2 == 0:
+            even_numb.append(numb)
+        elif numb % 2 == 1:
+            odd_numb.append(numb)
+    return (even_numb, odd_numb)
+
+assert even_or_odd([1, 2, 3, 4, 5]) == ([2, 4], [1, 3, 5])
+assert even_or_odd([45, 44, 102, 1045]) == ([44, 102], [45, 1045])
+assert even_or_odd([1, 3, 5, 7]) == ([], [1, 3, 5, 7])
+assert even_or_odd([1, 4.1, 2.2, 2.5])== ([], [1])
